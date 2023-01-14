076cb0a0946f54927bc12929204d7c83d25cd795
13gbc <118615125+13gbc@users.noreply.github.com>
2023-01-14 15:55:41 +0000

    Update get-securitytxt.sh

--- get-securitytxt.sh
+++ get-securitytxt.sh
@@ -6,7 +6,7 @@ regex='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$'
 while getopts ":i" opt; do
   case $opt in
     i)
-      echo "security.txt is a file that provides contact information for security researchers who have discovered vulnerabilities on a website. The file is typically located at the root of the domain or at .well-known/security.txt. It is a standard proposed by the Internet Bug Bounty organization, which aims to make it easier for researchers to report vulnerabilities and for website owners to receive and respond to reports."
+      echo "security.txt is a file that provides contact information for security researchers who have discovered vulnerabilities on a website. The file is typically located at the root of the domain or at .well-known/security.txt. It is a standard proposed by the Internet Bug Bounty organization, which aims to make it easier for researchers to report vulnerabilities and for website owners to receive and respond to reports. Please see https://securitytxt.org/ for details of the specification of this file"
       exit 0
       ;;
     \?)
