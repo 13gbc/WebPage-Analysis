96f87202c74a594c5c7b88815e5f0d0aac1f8872
13gbc <118615125+13gbc@users.noreply.github.com>
2023-01-14 15:41:48 +0000

    Update get-securitytxt.sh

--- get-securitytxt.sh
+++ get-securitytxt.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-website=$1
-if ! [[ $website == *"https://"* ]]; then
-  website="https://${website}"
+# Regular expression to match domain names
+regex='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$'
+
+# Check if a valid domain name is passed as an argument
+if [[ $1 =~ $regex ]]; then
+    domain=$1
+else
+    echo "Invalid domain name"
+    exit 1
 fi
 
-if curl --output /dev/null --silent --head --fail "${website}/security.txt"; then
-  echo "Displaying ${website}/security.txt"
-  curl "${website}/security.txt"
-elif curl --output /dev/null --silent --head --fail "${website}/.well-known/security.txt"; then
-  echo "Displaying ${website}/.well-known/security.txt"
-  curl "${website}/.well-known/security.txt"
-else
-  echo "security.txt file not found on website"
-fi
+# Download the files and store the results in variables
+security_txt=$(curl -s https://$domain/security.txt)
+well_known_security_txt=$(curl -s https://$domain/.well-known/security.txt)
+
+# Print the results with the URL title above it
+echo "https://$domain/security.txt:"
+echo "$security_txt"
+echo ""
+echo "https://$domain/.well-known/security.txt:"
+echo "$well_known_security_txt"
